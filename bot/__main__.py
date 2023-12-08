fb978d2fd1d7eb5a5d0ee94295694b5e1083ed90
reaperbots <148609775+reaperbots@users.noreply.github.com>
2023-12-08 14:04:04 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -254,10 +254,10 @@ async def start(_, message):
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
                        'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Z_Mirror'
+                       'Use me at: @Reaperzclub
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
-                       'Join: @Z_Mirror to use me.\n' \
+                       'Join: @Reaperzclub to use me.\n' \
                        'Thank You'
     else:
         tag = message.from_user.mention
