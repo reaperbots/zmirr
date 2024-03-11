4ca87314d54d05ebc5eeb8ef9f6b15c01073a046
Mr Reap <reapmr3@gmail.com>
2024-03-11 01:37:45 +0000

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -137,7 +137,7 @@ def get_readable_message():
             msg += f"\n📂 <blockquote>{escape(f'{download.name()}')}</blockquote>\n\n" if elapsed <= config_dict['AUTO_DELETE_MESSAGE_DURATION'] else ""
         else:
             msg += f"\n<code>{escape(f'{download.name()}')}</code>\n\n"
-        msg += f"<b>┌{download.status()} : {download.speed()}</b>"
+        msg += f"<b>┌{download.status()}  : {download.speed()}</b>"
         if download.status() not in [MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_PAUSED,
                                      MirrorStatus.STATUS_QUEUEDL, MirrorStatus.STATUS_QUEUEUP]:
             msg += f"\n├{get_progress_bar_string(download.progress())}"
