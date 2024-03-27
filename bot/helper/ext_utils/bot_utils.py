46833154f0b9a7ab8c7c8c7417e888db25d057fa
reaperbots <148609775+reaperbots@users.noreply.github.com>
2024-03-27 12:44:32 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -189,7 +189,7 @@ def get_readable_message():
             dl_speed += speed_in_bytes_per_second
         elif tstatus == MirrorStatus.STATUS_UPLOADING or tstatus == MirrorStatus.STATUS_SEEDING:
             up_speed += speed_in_bytes_per_second
-    msg += f"<b>UPTM: {get_readable_time(time() - botStartTime)} | FREE: {get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}"
+    msg += f"<b>UPTM:</b> {get_readable_time(time() - botStartTime)} <b>| FREE:</b> {get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}"
     if tasks <= STATUS_LIMIT:
         buttons = ButtonMaker()
         buttons.ibutton("REAPER BOT INFO", "status stats")
